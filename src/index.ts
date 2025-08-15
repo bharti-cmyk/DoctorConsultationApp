bb173ce876e073d354ec049e7bc9cea73afb45ec
Bharti <bhartimishra1993@gmail.com>
2025-08-15 11:58:40 +0530

    Check git actions

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}!!`);
 });
